afad7a7b9ee95c7269691badc5fac14ea347e51f
Cdj <ycdjonline@gmail.com>
2024-04-09 20:39:19 -0300

    criando models.py

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
 
+
 def index(request):
-    return render(request, 'galeria/index.html')
+
+
+    dados = {
+        1: {"nome": "Monster Hunter",
+            "plataforma": "PS4 / PC / SWITCH "},
+        2: {"nome": "Satisfactory",
+            "plataforma": "PC"}
+    }
+
+
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
