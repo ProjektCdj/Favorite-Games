9ca1dea1e15f26989a460ac99398b11a41a7f0c7
Cdj <ycdjonline@gmail.com>
2024-04-10 01:31:22 -0300

    migracoes sendo feitas

--- galeria/views.py
+++ galeria/views.py
@@ -1,19 +1,8 @@
 from django.shortcuts import render
 
 
-
 def index(request):
-
-
-    dados = {
-        1: {"nome": "Monster Hunter",
-            "plataforma": "PS4 / PC / SWITCH "},
-        2: {"nome": "Satisfactory",
-            "plataforma": "PC"}
-    }
-
-
-    return render(request, 'galeria/index.html', {"cards": dados})
+    return render(request, 'galeria/index.html')
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
